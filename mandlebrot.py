365bb7bc6882621e6407456d7b218e8679ca45d6
Neeserg Parajuli <neesergparajuli@gmail.com>
2023-05-20 16:30:35 +1000

    COmmiting old work, wasn't visible on github

--- mandlebrot.py
+++ mandlebrot.py
@@ -5,21 +5,24 @@ from PIL import Image
 import matplotlib.pyplot as plt
 DIMENSION = 10000
 NUM_ITER = 100
-
+upper_imaginary = 2
+lower_imgaginary = -2
+lower_real = -2.8
+upper_real = 1.2
 def make_image(image_array):
     image_array = deepcopy(image_array+1)
     image_array = image_array/(NUM_ITER+2)
     colormap = plt.get_cmap('magma')
     colors = np.uint8(colormap(image_array)*255)
     image = Image.fromarray(colors)
-    with open('mandlebrot.png','wb')as f:
+    with open('mandlebrot2.png','wb')as f:
         image.save(f,format='png')
 
 
 
-x = np.linspace(-2,2,DIMENSION)
+x = np.linspace(lower_real,upper_real,DIMENSION)
 x = np.tile(x,(DIMENSION,1))
-y = 1j*np.linspace(-2,2,DIMENSION)
+y = 1j*np.linspace(lower_imgaginary,upper_imaginary,DIMENSION)
 y = np.tile(y,(DIMENSION,1)).T
 
 c = x+y
@@ -35,8 +38,6 @@ for i in range(0,NUM_ITER):
     magnitude = np.abs(z)
     image_array[magnitude<1] = -1
     image_array[magnitude>2] = np.maximum(image_array[magnitude>2],NUM_ITER-i)
-    if i%20 == 0:
-        make_image(image_array=image_array)
 make_image(image_array=image_array)
 # magnitude[magnitude<1] = 0.0
 # magnitude[magnitude>2] = 2.0
